06d0a2e31a251e0fab998f1995fd7994c173e541
AliKamal <alikamal2381@gmail.com>
2021-04-03 00:23:54 +0500

    second-ccommit

---
+++ batch.bat
@@ -0,0 +1,7 @@
+echo "# multi-step-formik-10" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/alikamal2381/multi-step-formik-10.git
+git push -u origin main
